035cbea3c9ff401086de82970f74004e6ad98cbf
Jhenrique44 <jo-henrique8@hotmail.com>
2025-07-04 19:26:26 -0300

    feat: first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "It-trasacao-api"
---
+++ src/test/java/com/javanauta/It_trasacao_api/ItTrasacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.It_trasacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItTrasacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
