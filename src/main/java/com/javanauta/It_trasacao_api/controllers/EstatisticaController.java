abb47c90dc8cb60a9e40f485d877447d0a0c6035
Jhenrique44 <jo-henrique8@hotmail.com>
2025-07-07 15:46:03 -0300

    feat: add Swagger annotations for API documentation in EstatisticaController and TransacaoController

--- src/main/java/com/javanauta/It_trasacao_api/controllers/EstatisticaController.java
+++ src/main/java/com/javanauta/It_trasacao_api/controllers/EstatisticaController.java
@@ -1,6 +1,7 @@
 package com.javanauta.It_trasacao_api.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.javanauta.It_trasacao_api.business.services.EstatisticasService;
 import com.javanauta.It_trasacao_api.controllers.dto.EstatisticasResponseDTO;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -17,6 +21,13 @@ public class EstatisticaController {
 
     private final EstatisticasService estatisticaService;
 
+    @GetMapping
+    @Operation(description = "Endpoint responsavel por buscar as estatísticas das transações")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso!"),
+        @ApiResponse(responseCode = "400", description = "Erro na busca de estatísticas de transações"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<EstatisticasResponseDTO> buscarEstatisticas(
         @RequestParam(value = "intervaloBusca", required = false, defaultValue = "60") Integer intervaloBusca){
             return ResponseEntity.ok(
--- src/main/java/com/javanauta/It_trasacao_api/controllers/TransacaoController.java
+++ src/main/java/com/javanauta/It_trasacao_api/controllers/TransacaoController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.javanauta.It_trasacao_api.business.services.TransacaoService;
 import com.javanauta.It_trasacao_api.controllers.dto.TransacaoRequestDTO;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -19,12 +22,25 @@ public class TransacaoController {
 
     private final TransacaoService transacaoService;
     @PostMapping
+    @Operation(description = "Endpoint para registrar uma transação")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Transação registrada com sucesso"),
+        @ApiResponse(responseCode = "422", description = "Erro de validação, dados"),
+        @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> registrarTransacao(@RequestBody TransacaoRequestDTO dto) {
         transacaoService.registrarTransacao(dto);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
+    @Operation(description = "Endpoint para deletar transações")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Transação deletada com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> deleteTransacao(){ 
         transacaoService.deleteTransacao();
         return ResponseEntity.ok().build();
